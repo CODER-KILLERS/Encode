16e9dc8f684d297edc927982ea18a51b274cdacd
CODER-KILLERS <89589403+CODER-KILLERS@users.noreply.github.com>
2022-01-04 18:51:23 +0700

    Add files via upload

---
+++ tes_enc.py
@@ -0,0 +1,16 @@
+
+# Obfuscated By TAHMID RAYAT | HTR-TECH
+# Github    : https://www.github.com/htr-tech
+# Instagram : https://instagr.am/tahmid.rayat
+# --------------------------------------------
+# Time      : Tue Jan  4 18:50:13 2022
+# Platform  : Linux aarch64
+# --------------------------------------------
+
+import codecs,base64
+htr = [99, 72, 74, 112, 98, 110, 81, 103, 75, 67, 74, 111, 90, 87, 120]		
+tahmid = 'folO3o3WxVvxX'		
+pizza = '\x72\x6f\x74\x5f\x31\x33'		
+mobile = codecs.decode(eval('\x74\x61\x68\x6d\x69\x64'), eval('\x70\x69\x7a\x7a\x61'))		
+burger = base64.b64decode(''.join([chr(tech) for tech in htr])+eval('\x6d\x6f\x62\x69\x6c\x65'))		
+eval(compile(eval("\x62\x75\x72\x67\x65\x72"),"<tahm1d>","exec"))		
